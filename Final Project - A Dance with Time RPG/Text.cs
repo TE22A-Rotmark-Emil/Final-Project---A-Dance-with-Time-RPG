09db32fae7afcf9984c5e04ce863e142cdf38a95
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-02-21 10:55:59 +0100

    Added more junk to RemoveJunk

--- Final Project - A Dance with Time RPG/Text.cs
+++ Final Project - A Dance with Time RPG/Text.cs
@@ -27,6 +27,33 @@ class Text(){
         text = text.Replace("#", "");
         text = text.Replace("¤", "");
         text = text.Replace("%", "");
+        text = text.Replace("+", "");
+        text = text.Replace("@", "");
+        text = text.Replace("£", "");
+        text = text.Replace("$", "");
+        text = text.Replace("€", "");
+        text = text.Replace("&", "");
+        text = text.Replace("/", "");
+        text = text.Replace("{", "");
+        text = text.Replace("(", "");
+        text = text.Replace("[", "");
+        text = text.Replace(")", "");
+        text = text.Replace("]", "");
+        text = text.Replace("=", "");
+        text = text.Replace("}", "");
+        text = text.Replace("\\", "");
+        text = text.Replace("§", "");
+        text = text.Replace("½", "");
+        text = text.Replace("0", "");
+        text = text.Replace("1", "");
+        text = text.Replace("2", "");
+        text = text.Replace("3", "");
+        text = text.Replace("4", "");
+        text = text.Replace("5", "");
+        text = text.Replace("6", "");
+        text = text.Replace("7", "");
+        text = text.Replace("8", "");
+        text = text.Replace("9", "");
         return text;
     }
 }
