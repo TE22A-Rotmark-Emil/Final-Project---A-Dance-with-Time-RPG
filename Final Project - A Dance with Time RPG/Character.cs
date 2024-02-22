b47a47420677ecb1ce4570abc27109138986a2ef
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-02-22 13:05:37 +0100

    Added stats to the Character class

--- Final Project - A Dance with Time RPG/Character.cs
+++ Final Project - A Dance with Time RPG/Character.cs
@@ -1,3 +1,8 @@
 public class Character(){
-    
+    public string name;
+    public int attack;
+    public int defence;
+    public int dodgeChance;
+    public int accuracy = 100;
+    public bool isDead = false;
 }
