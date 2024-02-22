9edfe7db174bfc195b4379d6e07d54c5bd411ed9
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-02-22 12:07:53 +0100

    Made the checks for text speed part of the text class, fixed weird check for whether a choice had been made

--- Final Project - A Dance with Time RPG/Program.cs
+++ Final Project - A Dance with Time RPG/Program.cs
@@ -17,58 +17,18 @@ if (File.Exists("PersistentChoice.txt")){
     Console.Write(textSpeed);
     Console.WriteLine();
 }
-while (textSpeed == 1 || textSpeed == 2){
-    textSpeed = DecideTextSpeed(textSpeed);
+
+if (!File.Exists("PersistentChoice.txt")){
+    Text.NoSpeedPreference();
+    Text.DecideTextSpeed();
 }
+
 File.WriteAllText("PersistentChoice.txt", "TxtSpd: " + Convert.ToString(textSpeed));
 
 Gragerfourth.ShopkeeperDialogue("What are you doing here? These brats, they're always trying to take my stuff!", textSpeed);
 string test = Console.ReadLine().ToLower();
 if (test == "delete"){
     File.Delete("PersistentChoice.txt");
-}
-
-static int DecideTextSpeed(int textSpeed){
-    Shopkeeper TestDummy = new();
-    if (textSpeed == 1){
-        Console.WriteLine("Would you like a fast, balanced, or slow dialogue speed?");
-        Thread.Sleep(750);
-        Console.WriteLine("Example:");
-        TestDummy.ShopkeeperDialogue("This dialogue is written quickly, and can be difficult to keep up with.", 3);
-        TestDummy.ShopkeeperDialogue("This dialogue is written at a balanced pace, mildly faster than the average person reads.", 25);
-        TestDummy.ShopkeeperDialogue("This dialogue is written slowly, at around the pace the average person reads.", 35);
-    }
-    string desiredSpeed = Console.ReadLine().ToLower();
-    desiredSpeed = Text.RemoveJunk(desiredSpeed);
-    if (desiredSpeed is "fast" or "quick" or "quickly" or "speedy"){
-        textSpeed = 3;
-    }
-    else if (desiredSpeed is "balanced" or "medium" or "decent"){
-        textSpeed = 25;
-    }
-    else if (desiredSpeed is "slow" or "slower" or "slowly"){
-        textSpeed = 35;
-    }
-    else if (desiredSpeed is "instant" or "superfast"){
-        textSpeed = 0;
-    }
-    else if (desiredSpeed is "snail" or "veryslow" or "uninstant"){
-        textSpeed = 150;
-    }
-    else{
-        textSpeed = 2;
-        Text.ColourText(desiredSpeed, ConsoleColor.Yellow);
-        Console.Write(" is ");
-        Text.ColourText("invalid", ConsoleColor.Red);
-        Console.Write(". Please write ");
-        Text.ColourText("'quick', ", ConsoleColor.Cyan);
-        Text.ColourText("'balanced', ", ConsoleColor.Green);
-        Console.Write("or ");
-        Text.ColourText("'slow'", ConsoleColor.DarkGray);
-        Console.Write(" instead.");
-        Console.WriteLine();
-    }
-    return textSpeed;
 }
 
 static int ReadPersistence(string identifier){
--- Final Project - A Dance with Time RPG/Text.cs
+++ Final Project - A Dance with Time RPG/Text.cs
@@ -56,4 +56,53 @@ class Text(){
         text = text.Replace("9", "");
         return text;
     }
+
+    public static void NoSpeedPreference(){
+        Shopkeeper TestDummy = new();
+        Console.WriteLine("Would you like a fast, balanced, or slow dialogue speed?");
+        Thread.Sleep(750);
+        Console.WriteLine("Example:");
+        TestDummy.ShopkeeperDialogue("This dialogue is written quickly, and can be difficult to keep up with.", 3);
+        TestDummy.ShopkeeperDialogue("This dialogue is written at a balanced pace, mildly faster than the average person reads.", 25);
+        TestDummy.ShopkeeperDialogue("This dialogue is written slowly, at around the pace the average person reads.", 35);
+    }
+
+    public static int DecideTextSpeed(){
+        int textSpeed = 0;
+        bool choice = false;
+        while (choice == false){            
+            string desiredSpeed = Console.ReadLine().ToLower();
+            desiredSpeed = RemoveJunk(desiredSpeed);
+            choice = true;
+            if (desiredSpeed is "fast" or "quick" or "quickly" or "speedy"){
+                textSpeed = 3;
+            }
+            else if (desiredSpeed is "balanced" or "medium" or "decent"){
+                textSpeed = 25;
+            }
+            else if (desiredSpeed is "slow" or "slower" or "slowly"){
+                textSpeed = 35;
+            }
+            else if (desiredSpeed is "instant" or "superfast"){
+                textSpeed = 0;
+            }
+            else if (desiredSpeed is "snail" or "veryslow" or "uninstant"){
+                textSpeed = 150;
+            }
+            else{
+                ColourText(desiredSpeed, ConsoleColor.Yellow);
+                Console.Write(" is ");
+                ColourText("invalid", ConsoleColor.Red);
+                Console.Write(". Please write ");
+                ColourText("'quick', ", ConsoleColor.Cyan);
+                ColourText("'balanced', ", ConsoleColor.Green);
+                Console.Write("or ");
+                ColourText("'slow'", ConsoleColor.DarkGray);
+                Console.Write(" instead.");
+                Console.WriteLine();
+                choice = false;
+            }
+        }
+        return textSpeed;
+    }
 }
