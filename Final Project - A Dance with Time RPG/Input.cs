051c01458362b8ad1c80d0c918eadb8d535aafbb
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-03-11 09:07:03 +0100

    Added tests and checks for a joke encounter in the Paige dialogue, added support for a validInput count of 1 or 2, allowing for prompts where the player only has 1 or 2 options.

--- Final Project - A Dance with Time RPG/Input.cs
+++ Final Project - A Dance with Time RPG/Input.cs
@@ -9,16 +9,24 @@ public class Input(){
             }
         }
         while (validInput == false){
-            Text.ColourText("Invalid Input, please input ", ConsoleColor.Gray);
-            int i = 0;
-            int totalLength = validInputs.Count;
-            foreach (string a in validInputs){
-                i++;
-                if (i == totalLength){
-                    Text.ColourTextline("or " + a + ".", ConsoleColor.Gray);
-                }
-                else{
-                    Text.ColourText(a + ", ", ConsoleColor.Gray);
+            if (validInputs.Count == 2){
+                Text.ColourText("Invalid input, please input 1 or 2.", ConsoleColor.Gray);
+            }
+            else if (validInputs.Count == 1){
+                Text.ColourText("Invalid input, please input 1.", ConsoleColor.Gray);
+            }
+            else{
+                Text.ColourText("Invalid input, please input ", ConsoleColor.Gray);
+                int i = 0;
+                int totalLength = validInputs.Count;
+                foreach (string a in validInputs){
+                    i++;
+                    if (i == totalLength){
+                        Text.ColourTextline("or " + a + ".", ConsoleColor.Gray);
+                    }
+                    else{
+                        Text.ColourText(a + ", ", ConsoleColor.Gray);
+                    }
                 }
             }
             input = Console.ReadLine();
--- Final Project - A Dance with Time RPG/Interaction.cs
+++ Final Project - A Dance with Time RPG/Interaction.cs
@@ -19,9 +19,9 @@ public class Interaction(){
             if (response == ""){
                 Text.ColourTextline("How do you respond?", ConsoleColor.Cyan);
                 Text.ColourTextline("1. Who are you?", ConsoleColor.DarkCyan);
-                Text.ColourTextline("2. I don't know my name, sorry.", ConsoleColor.DarkCyan);
-                Text.ColourTextline("3. It'd be better if you avoided asking questions.", ConsoleColor.DarkRed);
-                Text.ColourTextline("4. That's none of your business.", ConsoleColor.DarkRed);
+                Text.ColourTextline("2. I don't know my name, sorry", ConsoleColor.DarkCyan);
+                Text.ColourTextline("3. It'd be better if you avoided asking questions", ConsoleColor.DarkRed);
+                Text.ColourTextline("4. That's none of your business", ConsoleColor.DarkRed);
                 validResponse = new(){"1", "2", "3", "4"};
                 response = Console.ReadLine();
                 response = Input.CheckValid(response, validResponse);
@@ -32,8 +32,8 @@ public class Interaction(){
             }
             if (response == "who are you?"){
                 Character.Talk(dialoguePartner, "I'm Paige, I'm an adventurer. Again, though, what is your name?");
-                Text.ColourTextline("1. I don't know my name, sorry.", ConsoleColor.DarkCyan);
-                Text.ColourTextline("2. That's none of your business.", ConsoleColor.DarkRed);
+                Text.ColourTextline("1. I don't know my name, sorry", ConsoleColor.DarkCyan);
+                Text.ColourTextline("2. That's none of your business", ConsoleColor.DarkRed);
                 validResponse = new(){"1", "2"};
                 response = Console.ReadLine();
                 response = Input.CheckValid(response, validResponse);
@@ -42,11 +42,11 @@ public class Interaction(){
             }
             else if (response == "no name, paige unknown"){
                 Character.Talk(Unknown, "Er? You sure?");
-                Character.Act(Unknown, "tilt their head, then places their scalp into the palm of their hand");
+                Character.Act(Unknown, "tilt their head, then place their scalp into the palm of their hand");
                 Character.Talk(Unknown, "How about this, I tell you my name, and you tell me yours, m'kay?");
                 Character.Talk(dialoguePartner, "I'm Paige, I'm an adventurer. So, with that out of the way, what is your name?");
-                Text.ColourTextline("1. I don't know my name, sorry.", ConsoleColor.DarkCyan);
-                Text.ColourTextline("2. That's none of your business.", ConsoleColor.DarkRed);
+                Text.ColourTextline("1. I really don't know my name", ConsoleColor.DarkCyan);
+                Text.ColourTextline("2. That's none of your business", ConsoleColor.DarkRed);
                 validResponse = new(){"1", "2"};
                 response = Console.ReadLine();
                 response = Input.CheckValid(response, validResponse);
@@ -61,14 +61,14 @@ public class Interaction(){
                 Text.ColourTextline("1. Superguy", ConsoleColor.Yellow);
                 Text.ColourTextline("2. Superboy", ConsoleColor.Yellow);
                 Text.ColourTextline("3. Supergirl", ConsoleColor.Yellow);
-                Text.ColourTextline("4. Glageroth, the Emanator of the Corruption, the 4th Descendant.", ConsoleColor.Yellow);
+                Text.ColourTextline("4. Glageroth, the Emanator of the Corruption, the 4th Descendant", ConsoleColor.Yellow);
                 Text.ColourTextline("5. Charlie", ConsoleColor.Yellow);
                 response = Console.ReadLine();
                 response = "blanked out";
             }
             if (response == "blanked out"){
-                Character.Act(MC, "couldn't think of anything. You simply stood there and shrugged.");
-                Character.Act(dialoguePartner, "simply sighs at your response.");
+                Character.Act(MC, "couldn't think of anything. You simply stood there and shrugged");
+                Character.Act(dialoguePartner, "simply sighs at your response");
                 Character.Talk(dialoguePartner, "Well, that's a first.");
                 Character.Talk(dialoguePartner, "Why are you out here, then? Trying to figure out who you are?");
                 Text.ColourTextline("1. Yea", ConsoleColor.DarkCyan);
@@ -79,28 +79,40 @@ public class Interaction(){
                 response = Console.ReadLine();
                 response = Input.CheckValid(response, validResponse);
                 if (response == "1" || response == "2"){response = "figuring myself out";}
-                else{response = "not figuring myself out /j";}
+                else{response = "not figuring myself out";}
             }
-            if (response == "not figuring myself out /j"){
+            if (response == "not figuring myself out"){
                 Character.Act(dialoguePartner, "raises her eyebrow and looks intently at you");
                 Character.Talk(dialoguePartner, "What? Why are you here then?");
-                while (response == "not figuring myself out /j"){
-                    Text.ColourTextline("1. I was just joking, I plan on figuring out who I am.", ConsoleColor.DarkCyan);
-                    Text.ColourTextline("2. I'm seeking the Sorceror's Stone, and I intend on finding it. Intel seems to suggest that it is located between 38.897957N, 77.036560W and 51.503368N, 0.127721W", ConsoleColor.Yellow);
-                    Text.ColourTextline("3. That's none of your business.", ConsoleColor.DarkRed);
-                    validResponse = new(){"1", "2", "3"};
+                bool hasPickedJokeAnswer = false;
+                while (response == "not figuring myself out"){
+                    Text.ColourTextline("1. I was just joking, I plan on figuring out who I am", ConsoleColor.DarkCyan);
+                    if (hasPickedJokeAnswer == false){
+                        Text.ColourTextline("2. I'm seeking the Sorceror's Stone, and I intend on finding it. Intel seems to suggest that it is located between 38.897957N, 77.036560W and 51.503368N, 0.127721W", ConsoleColor.Yellow);
+                    }
+                    if (hasPickedJokeAnswer == false){
+                        Text.ColourTextline("3. That's none of your business", ConsoleColor.DarkRed);
+                    }
+                    else{
+                        Text.ColourTextline("2. That's none of your business", ConsoleColor.DarkRed);
+                    }
+                    if (hasPickedJokeAnswer == false){
+                        validResponse = new(){"1", "2", "3"};
+                    }
+                    else{
+                        validResponse = new(){"1", "2"};
+                    }
                     response = Console.ReadLine();
                     response = Input.CheckValid(response, validResponse);
                     if (response == "2"){
-                        response = "not figuring myself out";
+                        if (hasPickedJokeAnswer == false){    
+                            hasPickedJokeAnswer = true;
+                            response = "not figuring myself out";
+                        }
+                        else{
+                            response = "no new party member, paige known";
+                        }
                     }
-                }
-                if (response == "not figuring myself out"){
-                    Text.ColourTextline("1. I was just joking, I plan on figuring out who I am.", ConsoleColor.DarkCyan);
-                    Text.ColourTextline("2. That's none of your business.", ConsoleColor.DarkRed);
-                    validResponse = new(){"1", "2"};
-                    response = Console.ReadLine();
-                    response = Input.CheckValid(response, validResponse);
                 }
                 if (response == "1"){
                     Character.Talk(dialoguePartner, "What a... 'fine' sense of humour?");
